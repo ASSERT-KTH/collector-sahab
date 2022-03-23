5923f1c6b285e28fdf2d7c4796faeedf27585cf3
Aman Sharma <mannu.poski10@gmail.com>
2022-03-23 15:52:42 +0100

    chore: initialise with test (#13)

--- src/main/java/se/kth/debug/MethodTestRunner.java
+++ src/main/java/se/kth/debug/MethodTestRunner.java
@@ -1,22 +1,20 @@
 package se.kth.debug;
 
+import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.concurrent.*;
-import org.junit.internal.TextListener;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Request;
+import org.junit.platform.console.options.CommandLineOptions;
+import org.junit.platform.console.tasks.ConsoleTestExecutor;
 
 public class MethodTestRunner {
     public static void main(String... args) {
         String[] tests = args[0].split(" ");
-        for (int i = 0; i < tests.length; i++) {
-            final String test = tests[i];
+        for (final String test : tests) {
             ExecutorService executor = Executors.newSingleThreadExecutor();
             Callable<Object> task =
-                    new Callable<Object>() {
-                        public Object call() {
-                            runTest(test);
-                            return null;
-                        }
+                    () -> {
+                        runTest(test);
+                        return 0;
                     };
             Future<Object> future = executor.submit(task);
             try {
@@ -31,14 +29,10 @@ public class MethodTestRunner {
         }
     }
 
-    private static void runTest(String test) {
-        try {
-            Request request = Request.classes(Class.forName(test));
-            JUnitCore junit = new JUnitCore();
-            junit.addListener(new TextListener(System.out));
-            junit.run(request);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    private static void runTest(String test) throws Exception {
+        CommandLineOptions options = new CommandLineOptions();
+        options.setSelectedClasses(Collections.singletonList(test));
+        options.setFailIfNoTests(true);
+        new ConsoleTestExecutor(options).execute(new PrintWriter(System.out));
     }
 }
---
+++ src/test/resources/sample-maven-project/src/main/java/foo/BasicMath.java
@@ -0,0 +1,11 @@
+package foo;
+
+public class BasicMath {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+}
