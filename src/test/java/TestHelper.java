1113c886cef5233832c4cec77f46cd26a10aeb3e
Aman Sharma <mannu.poski10@gmail.com>
2022-04-08 16:34:52 +0200

    Skip collection of non-static fields of classes from static method

--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -1,18 +1,17 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 
 public class TestHelper {
     public static final Path PATH_TO_SAMPLE_MAVEN_PROJECT =
             Paths.get("src/test/resources/sample-maven-project");
 
-    public static String getMavenClasspathFromBuildDirectory(Path buildDirectory) {
+    public static String[] getMavenClasspathFromBuildDirectory(Path buildDirectory) {
         List<String> classpath =
                 List.of(
                         buildDirectory.resolve("classes").toString(),
                         buildDirectory.resolve("test-classes").toString(),
                         buildDirectory.resolve("dependency").toString());
-        return StringUtils.join(classpath, " ");
+        return classpath.toArray(new String[] {});
     }
 }
