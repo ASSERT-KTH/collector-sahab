74176a9d913191a960014e27b47fcf55e678271d
Aman Sharma <mannu.poski10@gmail.com>
2022-05-13 18:05:28 +0200

    Verify matched lines for multiple nested anonymous classes

---
+++ src/test/resources/matched-line-finder/nested-anonymous-class/right.java
@@ -0,0 +1,27 @@
+public class NestedAnonymousClass {
+    public class TopLevelAnonymous { }
+    public class Anonymous1 { }
+    public class Anonymous2 { }
+
+    public String makeCodeUnreadable() {
+        TopLevelAnonymous tla = new TopLevelAnonymous() {
+            @Override
+            public String toString() {
+                Anonymous1 anonymous1 = new Anonymous1() {
+                    @Override
+                    public String toString() {
+                        Anonymous2 anonymous2 = new Anonymous2() {
+                            @Override
+                            public String toString() {
+                                return "give me some rain";
+                            }
+                        }
+                        return anonymous2.toString();
+                    }
+                }
+                return anonymous1.toString();
+            }
+        }
+        return tla.toString();
+    }
+}
