a33c5f940974b94f5fe7dbf3ffc2b5861dc35163
Aman Sharma <mannu.poski10@gmail.com>
2022-07-02 17:58:10 +0530

    refactor: rewrite MatchedLineFinder to find breakpoints using UNIX diff instead of AST diff (#93)
    * Modify tests to get expected results from rewritten MLF
    
    * Add `toString` for MethodForExitEvent
    
    * Add script to compute diff lines
    
    * Throw exception where there is no diff
    
    * Fix test - patch3-Math-50-Jaid-plausible
    
    * Fix tests
    
    * Install gawk
    
    * Revert "Install gawk"
    
    This reverts commit da6a524ac784ceec734b26f69db01193c0425491.
    
    * Add expected files
    
    * Update prerequisites

--- src/test/resources/matched-line-finder/patch3-Math-50-Jaid-plausible/right.java
+++ src/test/resources/matched-line-finder/patch3-Math-50-Jaid-plausible/right.java
@@ -186,7 +186,7 @@ public abstract class BaseSecantSolver
                         // Nothing.
                         if (x == x1) {
                             if(!((f0 > x0 + x1) == true)){
-                                x0 = 0.5 * (x0 + x1 - FastMath.max(rtol * FastMath.abs(x1), atol));
+                            x0 = 0.5 * (x0 + x1 - FastMath.max(rtol * FastMath.abs(x1), atol));
                             }
                             f0 = computeObjectiveValue(x0);
                         }
