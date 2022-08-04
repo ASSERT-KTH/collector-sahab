0aa4b1f242cd60aa64a7c69ce0b7a6dc0ca542bd
Aman Sharma <mannu.poski10@gmail.com>
2022-08-04 15:28:40 +0200

    chore: run all JUnit tests in a single container (#97)

--- src/main/java/se/kth/debug/JUnitTestRunner.java
+++ src/main/java/se/kth/debug/JUnitTestRunner.java
@@ -4,53 +4,52 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectMethod;
 
 import java.io.PrintWriter;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.*;
 import org.junit.platform.console.options.CommandLineOptions;
 import org.junit.platform.console.tasks.ConsoleTestExecutor;
+import org.junit.platform.engine.discovery.ClassSelector;
+import org.junit.platform.engine.discovery.MethodSelector;
 
 public class JUnitTestRunner {
     public static void main(String... args) {
-        String[] tests = args[0].split(" ");
-        for (final String test : tests) {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            Callable<Object> task =
-                    () -> {
-                        if (test.contains("::")) {
-                            runTestMethod(test);
-                        } else {
-                            runTestClass(test);
-                        }
-                        return 0;
-                    };
-            Future<Object> future = executor.submit(task);
-            try {
-                executor.shutdown();
-                future.get(4, TimeUnit.MINUTES);
-            } catch (Exception ex) {
-                // handle other exceptions
-                ex.printStackTrace();
-            } finally {
-                future.cancel(true);
-            }
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<Object> task =
+                () -> {
+                    runTests(args[0].split(" "));
+                    return 0;
+                };
+        Future<Object> future = executor.submit(task);
+        try {
+            executor.shutdown();
+            future.get(4, TimeUnit.MINUTES);
+        } catch (Exception ex) {
+            // handle other exceptions
+            ex.printStackTrace();
+        } finally {
+            future.cancel(true);
         }
     }
 
-    private static void runTestMethod(String test) throws Exception {
-        String[] classAndMethod = test.split("::");
-        String clazz = classAndMethod[0];
-        String method = classAndMethod[1];
+    private static void runTests(String... tests) throws Exception {
+        Set<MethodSelector> methods = new HashSet<>();
+        Set<ClassSelector> classes = new HashSet<>();
+        for (final String test : tests) {
+            if (test.contains("::")) {
+                String[] classAndMethod = test.split("::");
+                String clazz = classAndMethod[0];
+                String method = classAndMethod[1];
 
+                methods.add(selectMethod(clazz + "#" + method));
+            } else {
+                classes.add(selectClass(test));
+            }
+        }
         CommandLineOptions options = new CommandLineOptions();
-        options.setSelectedMethods(Collections.singletonList(selectMethod(clazz + "#" + method)));
-        options.setFailIfNoTests(true);
-        options.setAnsiColorOutputDisabled(true);
-        new ConsoleTestExecutor(options).execute(new PrintWriter(System.out));
-    }
-
-    private static void runTestClass(String test) throws Exception {
-        CommandLineOptions options = new CommandLineOptions();
-        options.setSelectedClasses(Collections.singletonList(selectClass(test)));
+        options.setSelectedMethods(new ArrayList<>(methods));
+        options.setSelectedClasses(new ArrayList<>(classes));
         options.setFailIfNoTests(true);
         options.setAnsiColorOutputDisabled(true);
         new ConsoleTestExecutor(options).execute(new PrintWriter(System.out));
