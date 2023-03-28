ad0e1f6bd210399d69dab5ccadc19b41ba85753a
Aman Sharma <mannu.poski10@gmail.com>
2023-03-28 12:58:24 +0200

    Add test for the new collector

---
+++ src/test/java/NewCollectorTest.java
@@ -0,0 +1,60 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.junit.jupiter.api.Test;
+import se.assertteam.LineSnapshot;
+import se.assertteam.RuntimeValue;
+import se.assertteam.SahabOutput;
+import se.assertteam.StackFrameContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class NewCollectorTest {
+    @Test
+    void basicMath_test() throws MavenInvocationException, IOException {
+        // arrange
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(new File("src/test/resources/basic-math/pom.xml"));
+        request.setGoals(List.of("test"));
+
+        // act
+        Invoker invoker = new DefaultInvoker();
+        InvocationResult result = invoker.execute(request);
+
+        // assert
+        assertThat(result.getExitCode(), equalTo(1));
+        File actualOutput = new File("src/test/resources/basic-math/target/output.json");
+        assertThat(actualOutput.exists(), equalTo(true));
+
+        ObjectMapper mapper = new ObjectMapper();
+        SahabOutput output = mapper.readValue(actualOutput, new TypeReference<>() {});
+        assertThat(output.getBreakpoint().size(), equalTo(2));
+
+        LineSnapshot snapshot1 = output.getBreakpoint().get(0);
+        LineSnapshot snapshot2 = output.getBreakpoint().get(1);
+        assertThat(snapshot1.getLineNumber(), equalTo(5));
+        assertThat(snapshot2.getLineNumber(), equalTo(9));
+
+        // StackFrameContext
+        StackFrameContext theOnlyStackFrameContext = snapshot1.getStackFrameContext().get(0);
+        List<RuntimeValue> runtimeValues = theOnlyStackFrameContext.getRuntimeValueCollection();
+        RuntimeValue runtimeValue0 = runtimeValues.get(0);
+        assertThat(runtimeValue0.getValue(), equalTo(23));
+        assertThat(runtimeValue0.getType(), equalTo(int.class));
+
+        RuntimeValue runtimeValue1 = runtimeValues.get(1);
+        assertThat(runtimeValue1.getValue(), equalTo(2));
+        assertThat(runtimeValue1.getType(), equalTo(int.class));
+        // ToDo: Add feature for collecting return values
+    }
+}
